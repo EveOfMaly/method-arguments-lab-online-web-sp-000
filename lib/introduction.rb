9191ba927ae1da25420259d9217b00c1de3d6189
John Kyeremeh <john.a.kyeremeh@gmail.com>
2021-05-01 13:59:45 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -2,6 +2,6 @@ def introduction(name)
   puts "Hi, my name is #{name}."
 end
 
-def introduction_with_langage(name,langugage)
+def introduction_with_language(name,langugage)
   puts "Hi, my name is #{name} and I a learning to program in #{language}."
 end
